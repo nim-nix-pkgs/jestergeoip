4e4778348a78c74b194847c73bebce79e84430b6
hugosenari <hugosenari@gmail.com>
2022-05-12 11:53:02 -0300

    chore: re index jestergeoip

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."jestergeoip-master".dir   = "master";
+  inputs."jestergeoip-master".owner = "nim-nix-pkgs";
+  inputs."jestergeoip-master".ref   = "master";
+  inputs."jestergeoip-master".repo  = "jestergeoip";
+  inputs."jestergeoip-master".type  = "github";
+  inputs."jestergeoip-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jestergeoip-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."jestergeoip-v1_0_1".dir   = "v1_0_1";
+  inputs."jestergeoip-v1_0_1".owner = "nim-nix-pkgs";
+  inputs."jestergeoip-v1_0_1".ref   = "master";
+  inputs."jestergeoip-v1_0_1".repo  = "jestergeoip";
+  inputs."jestergeoip-v1_0_1".type  = "github";
+  inputs."jestergeoip-v1_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."jestergeoip-v1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
